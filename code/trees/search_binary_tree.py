2ff8a419baef9f0a82c5c635ef53f28cb7cbfa9b
Camden-Kirkpatrick <camdenckirkpatrick@gmail.com>
2026-03-31 14:04:34 -0500

    created search_binary_tree.py

--- code/trees/search_binary_tree.py
+++ code/trees/search_binary_tree.py
@@ -5,14 +5,14 @@ class TreeNode:
         self.right = None
 
 
-def search(root, target):
+def search_tree(root, target):
     if not root:
         return False
     
     if target > root.data:
-        return search(root.right, target)
+        return search_tree(root.right, target)
     elif target < root.data:
-        return search(root.left, target)
+        return search_tree(root.left, target)
     else:
         return True
 
@@ -25,6 +25,6 @@ if __name__ == "__main__":
     root.right = n2
 
     target = 13
-    result = search(root, target)
+    result = search_tree(root, target)
     print(f"{target} was {'found' if result else 'not found'}")
 
